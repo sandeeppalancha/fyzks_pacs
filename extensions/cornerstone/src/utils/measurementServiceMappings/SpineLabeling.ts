7b40af7cb7a4f81d9e7602d5ac2f21fbb8341fd6
Jilani Basha Pulluru <119941899+jilanibashaP@users.noreply.github.com>
2025-01-23 04:07:38 +0000

    Assistant checkpoint
    Assistant generated file changes:
    - extensions/cornerstone/src/utils/measurementServiceMappings/SpineLabeling.ts: Remove duplicate SpineLabeling declaration
    
    ---
    
    User prompt:
    
    measurementServiceMappings\SpineLabeling.ts: Identifier 'SpineLabeling' has already been declared. (69:6)

--- extensions/cornerstone/src/utils/measurementServiceMappings/SpineLabeling.ts
+++ extensions/cornerstone/src/utils/measurementServiceMappings/SpineLabeling.ts
@@ -1,5 +1,7 @@
-
-import { getSOPInstanceAttributes } from './utils/getSOPInstanceAttributes';
+import SUPPORTED_TOOLS from './constants/supportedTools';
+import { getDisplayUnit } from './utils';
+import getSOPInstanceAttributes from './utils/getSOPInstanceAttributes';
+import { utils } from '@ohif/core';
 
 const SpineLabeling = {
   toAnnotation: measurement => {
@@ -14,60 +16,6 @@ const SpineLabeling = {
       annotationUID: uid,
     };
   },
-
-  toMeasurement: (
-    csToolsEventDetail,
-    displaySetService,
-    CornerstoneViewportService,
-    getValueTypeFromToolType,
-    customizationService
-  ) => {
-    const { annotation, viewportId } = csToolsEventDetail;
-    const { metadata, data, annotationUID } = annotation;
-
-    if (!metadata || !data) {
-      console.warn('Spine Labeling tool: Missing metadata or data');
-      return null;
-    }
-
-    const { toolName, referencedImageId, FrameOfReferenceUID } = metadata;
-    const { SOPInstanceUID, SeriesInstanceUID, StudyInstanceUID } =
-      getSOPInstanceAttributes(
-        referencedImageId,
-        CornerstoneViewportService,
-        viewportId
-      );
-
-    let displaySet;
-
-    if (SOPInstanceUID) {
-      displaySet = displaySetService.getDisplaySetForSOPInstanceUID(
-        SOPInstanceUID,
-        SeriesInstanceUID
-      );
-    } else {
-      displaySet = displaySetService.getDisplaySetsForSeries(SeriesInstanceUID);
-    }
-
-    return {
-      uid: annotationUID,
-      SOPInstanceUID,
-      FrameOfReferenceUID,
-      toolName: annotation.metadata.toolName,
-      label: data.label,
-      coordinates: data.handles.points[0],
-    };
-  },
-};
-
-export default SpineLabeling;
-import SUPPORTED_TOOLS from './constants/supportedTools';
-import { getDisplayUnit } from './utils';
-import getSOPInstanceAttributes from './utils/getSOPInstanceAttributes';
-import { utils } from '@ohif/core';
-
-const SpineLabeling = {
-  toAnnotation: measurement => {},
 
   toMeasurement: (
     csToolsEventDetail,
