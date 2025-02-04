06febbdaffbcf89978749d4d2b503274a3c47aa5
Jilani Basha Pulluru <119941899+jilanibashaP@users.noreply.github.com>
2025-02-04 05:03:17 +0000

    Restored to '82c26032328a81c5c20065bb89bcfcdc0c7842b5'
    Replit-Restored-To:82c26032328a81c5c20065bb89bcfcdc0c7842b5

--- modes/longitudinal/src/initToolGroups.js
+++ modes/longitudinal/src/initToolGroups.js
@@ -1,13 +1,17 @@
-import { Enums } from '@cornerstonejs/tools';
+function initDefaultToolGroup(
+  extensionManager,
+  toolGroupService,
+  commandsManager,
+  toolGroupId,
+  modeLabelConfig
+) {
+  const utilityModule = extensionManager.getModuleEntry(
+    '@ohif/extension-cornerstone.utilityModule.tools'
+  );
 
-const toolGroupIds = {
-  CT: 'ctToolGroup',
-  default: 'default',
-  mpr: 'mpr',
-};
+  const { toolNames, Enums } = utilityModule.exports;
 
-function createTools(toolNames) {
-  return {
+  const tools = {
     active: [
       {
         toolName: toolNames.WindowLevel,
@@ -17,6 +21,10 @@ function createTools(toolNames) {
         toolName: toolNames.Pan,
         bindings: [{ mouseButton: Enums.MouseBindings.Auxiliary }],
       },
+      // {
+      //   toolName: toolNames.CustomDragTool,
+      //   bindings: [{ mouseButton: Enums.MouseBindings.Auxiliary }],
+      // },
       {
         toolName: toolNames.Zoom,
         bindings: [{ mouseButton: Enums.MouseBindings.Secondary }],
@@ -25,39 +33,60 @@ function createTools(toolNames) {
     ],
     passive: [
       { toolName: toolNames.Length },
+      {
+        toolName: toolNames.ArrowAnnotate,
+        configuration: {
+          getTextCallback: (callback, eventDetails) => {
+            if (modeLabelConfig) {
+              callback(' ');
+            } else {
+              commandsManager.runCommand('arrowTextCallback', {
+                callback,
+                eventDetails,
+              });
+            }
+          },
+          changeTextCallback: (data, eventDetails, callback) => {
+            if (modeLabelConfig === undefined) {
+              commandsManager.runCommand('arrowTextCallback', {
+                callback,
+                data,
+                eventDetails,
+              });
+            }
+          },
+        },
+      },
+      { toolName: toolNames.Bidirectional },
+      { toolName: toolNames.DragProbe },
+      // { toolName: toolNames.CustomDragTool },
       { toolName: toolNames.Probe },
       { toolName: toolNames.EllipticalROI },
+      { toolName: toolNames.CircleROI },
       { toolName: toolNames.RectangleROI },
       { toolName: toolNames.StackScroll },
-      { toolName: toolNames.Crosshairs },
+      { toolName: toolNames.Angle },
+      { toolName: toolNames.CobbAngle },
+      { toolName: toolNames.Magnify },
+      { toolName: toolNames.SegmentationDisplay },
+      { toolName: toolNames.CalibrationLine },
+
+      { toolName: toolNames.UltrasoundDirectional },
+      { toolName: toolNames.PlanarFreehandROI },
+      { toolName: toolNames.SplineROI },
+      { toolName: toolNames.LivewireContour },
+      { toolName: toolNames.SpineLabeling },
+      { toolName: toolNames.CTR },
+    ],
+    enabled: [{ toolName: toolNames.ImageOverlayViewer }, { toolName: toolNames.ReferenceLines }],
+    disabled: [
+      {
+        toolName: toolNames.AdvancedMagnify,
+      },
     ],
   };
-}
 
-function initDefaultToolGroup(extensionManager, toolNames) {
-  const { ToolGroupManager } = extensionManager.getModuleEntry(
-    '@ohif/extension-cornerstone.services.toolGroupService'
-  );
-
-  const toolGroup = ToolGroupManager.createToolGroup(toolGroupIds.default);
-
-  const tools = createTools(toolNames);
-
-  tools.active.forEach(tool => {
-    toolGroup.addTool(tool.toolName, {
-      bindings: tool.bindings,
-    });
-  });
-
-  tools.passive.forEach(tool => {
-    toolGroup.addTool(tool.toolName);
-  });
-
-  toolGroup.setToolActive(tools.active[0].toolName, {
-    bindings: tools.active[0].bindings,
-  });
-
-  return toolGroup;
+  toolGroupService.createToolGroupAndAddTools(toolGroupId, tools);
 }
 
 function initSRToolGroup(extensionManager, toolGroupService) {
@@ -145,6 +174,10 @@ function initMPRToolGroup(extensionManager, toolGroupService, commandsManager, m
         toolName: toolNames.Pan,
         bindings: [{ mouseButton: Enums.MouseBindings.Auxiliary }],
       },
+      // {
+      //   toolName: toolNames.CustomDragTool,
+      //   bindings: [{ mouseButton: Enums.MouseBindings.Auxiliary }],
+      // },
       {
         toolName: toolNames.Zoom,
         bindings: [{ mouseButton: Enums.MouseBindings.Secondary }],
@@ -179,6 +212,7 @@ function initMPRToolGroup(extensionManager, toolGroupService, commandsManager, m
       },
       { toolName: toolNames.Bidirectional },
       { toolName: toolNames.DragProbe },
+      // { toolName: toolNames.CustomDragTool },
       { toolName: toolNames.Probe },
       { toolName: toolNames.EllipticalROI },
       { toolName: toolNames.CircleROI },
@@ -211,7 +245,6 @@ function initMPRToolGroup(extensionManager, toolGroupService, commandsManager, m
 
   toolGroupService.createToolGroupAndAddTools('mpr', tools);
 }
-
 function initVolume3DToolGroup(extensionManager, toolGroupService) {
   const utilityModule = extensionManager.getModuleEntry(
     '@ohif/extension-cornerstone.utilityModule.tools'
@@ -239,33 +272,17 @@ function initVolume3DToolGroup(extensionManager, toolGroupService) {
   toolGroupService.createToolGroupAndAddTools('volume3d', tools);
 }
 
-export default function initToolGroups(extensionManager, commandsManager, servicesManager) {
-  const toolNames = {
-    WindowLevel: 'WindowLevel',
-    Pan: 'Pan', 
-    Zoom: 'Zoom',
-    StackScrollMouseWheel: 'StackScrollMouseWheel',
-    Length: 'Length',
-    Probe: 'Probe',
-    EllipticalROI: 'EllipticalROI', 
-    RectangleROI: 'RectangleROI',
-    StackScroll: 'StackScroll',
-    Crosshairs: 'Crosshairs',
-    ArrowAnnotate: 'ArrowAnnotate',
-    Bidirectional: 'Bidirectional',
-    DragProbe: 'DragProbe',
-    Angle: 'Angle',
-    CobbAngle: 'CobbAngle',
-    PlanarFreehandROI: 'PlanarFreehandROI',
-    SegmentationDisplay: 'SegmentationDisplay',
-    CTR: 'CTR',
-    AdvancedMagnify: 'AdvancedMagnify',
-    ReferenceLines: 'ReferenceLines',
-    TrackballRotateTool: 'TrackballRotateTool'
-  };
-
-  initDefaultToolGroup(extensionManager, toolNames);
-  initSRToolGroup(extensionManager, servicesManager);
-  initMPRToolGroup(extensionManager, servicesManager, commandsManager);
-  initVolume3DToolGroup(extensionManager, servicesManager);
+function initToolGroups(extensionManager, toolGroupService, commandsManager, modeLabelConfig) {
+  initDefaultToolGroup(
+    extensionManager,
+    toolGroupService,
+    commandsManager,
+    'default',
+    modeLabelConfig
+  );
+  initSRToolGroup(extensionManager, toolGroupService, commandsManager);
+  initMPRToolGroup(extensionManager, toolGroupService, commandsManager, modeLabelConfig);
+  initVolume3DToolGroup(extensionManager, toolGroupService);
 }
+
+export default initToolGroups;
